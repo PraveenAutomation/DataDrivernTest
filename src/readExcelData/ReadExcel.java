f9936041ea88cc2b3e6ba736050def67d11c874b
PraveenRepo <praveen.intelegencia@gmail.com>
2018-02-24 19:18:27 +0530

    DDF Project

---
+++ src/readExcelData/ReadExcel.java
@@ -0,0 +1,33 @@
+package readExcelData;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ReadExcel {
+
+	public static void main(String[] args) throws Exception {
+		
+		File src = new File("E:\\BULUT DRIVE\\SELENIUM VIDEOS -ALL\\Data Drivern Framework Mat\\ExcelData\\TestData.xlsx");
+		
+		FileInputStream fis = new FileInputStream(src);
+		
+		XSSFWorkbook wb = new XSSFWorkbook(fis);
+		XSSFSheet  sheet1= wb.getSheetAt(0);
+		
+		String data0=sheet1.getRow(0).getCell(0).getStringCellValue();
+		
+		System.out.println("Data from Excel: "+data0);
+		
+		String data1=sheet1.getRow(0).getCell(1).getStringCellValue();
+		
+		System.out.println("Data from Excel: "+data1);
+		
+		wb.close();
+
+	}
+
+}
---
+++ src/readExcelData/ReadExcel1.java
@@ -0,0 +1,33 @@
+package readExcelData;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ReadExcel1 {
+public static void main(String[] args) throws Exception {
+		
+		File src = new File("E:\\BULUT DRIVE\\SELENIUM VIDEOS -ALL\\Data Drivern Framework Mat\\ExcelData\\TestData.xlsx");
+		
+		FileInputStream fis = new FileInputStream(src);
+		
+		XSSFWorkbook wb = new XSSFWorkbook(fis);
+		XSSFSheet  sheet1= wb.getSheetAt(0);
+		
+		int rowCount=sheet1.getLastRowNum();
+		System.out.println("Total No. of rows: "+rowCount);
+		
+		for(int i=0;i<rowCount;i++)
+		{
+			String data0=sheet1.getRow(i).getCell(0).getStringCellValue();
+			System.out.println("Data from Excel: "+data0);
+		}
+		
+		
+		wb.close();
+
+	}
+
+}
